219762f54ee9f151e6bbff13571963cf2bcfddf9
artemustilov <artem.ustilov@gmail.com>
2020-03-05 18:41:07 +0200

    add prod build

---
+++ docs/precache-manifest.cf47e8b8de3a84b6485d54ce84611adc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "879e1f19269a727a0bcec5a43230573b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e444d1c306260f6f0f4a",
+    "url": "/static/css/main.03e904ca.chunk.css"
+  },
+  {
+    "revision": "1dfc3908174ce5b0f89d",
+    "url": "/static/js/2.1cdec997.chunk.js"
+  },
+  {
+    "revision": "d2cf8d37946cd02abf2fe73b5ccb09c4",
+    "url": "/static/js/2.1cdec997.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e444d1c306260f6f0f4a",
+    "url": "/static/js/main.2b146c3e.chunk.js"
+  },
+  {
+    "revision": "7f91457462a91975f36a",
+    "url": "/static/js/runtime-main.713b4222.js"
+  }
+]);
